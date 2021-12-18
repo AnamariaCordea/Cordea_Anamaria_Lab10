739ebc5d3b392d6a3ca4a042679de2202a08be0a
ANAMARIA CORDEA <anamaria.cordea@stud.ubbcluj.ro>
2021-12-18 17:05:35 +0200

    L10

--- Cordea_Anamaria_Lab10/App.xaml.cs
+++ Cordea_Anamaria_Lab10/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Cordea_Anamaria_Lab10.Services;
 using Cordea_Anamaria_Lab10.Views;
 using System;
+using Cordea_Anamaria_Lab10.Data;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,11 +10,25 @@ namespace Cordea_Anamaria_Lab10
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
 
         public App()
         {
             InitializeComponent();
-
+            MainPage = new NavigationPage(new ListEntryPage());
             DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
         }
