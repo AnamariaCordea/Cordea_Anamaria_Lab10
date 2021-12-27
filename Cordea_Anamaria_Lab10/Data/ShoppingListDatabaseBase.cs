54d0eedf5930f53420f79e37a3cf6963160d291c
ANAMARIA CORDEA <anamaria.cordea@stud.ubbcluj.ro>
2021-12-27 14:22:49 +0200

    L11

---
+++ Cordea_Anamaria_Lab10/Data/ShoppingListDatabaseBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Cordea_Anamaria_Lab10.Data
+{
+    public class ShoppingListDatabaseBase
+    {
+        public Task<List<Product>> GetListProductsAsync(int shoplistid)
+        {
+            return _database.QueryAsync<Product>(
+            "select P.ID, P.Description from Product P"
+            + " inner join ListProduct LP"
+            + " on P.ID = LP.ProductID where LP.ShopListID = ?",
+            shoplistid);
+        }
+    }
+}
